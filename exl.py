b958e1231dd006f7749a0fd571fe1316bf95fb84
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-05-26 02:54:58 +0300

    Добавил чтение из Excel файла
    использовал библиотеку openpyxl

---
+++ exl.py
@@ -0,0 +1,42 @@
+#import openpyxl as excl
+from openpyxl import load_workbook
+import numpy as np
+
+filename1 = "Компетенции.xlsx"
+filename2 = "load.txt"
+
+
+word = input()
+filename = "load.txt"
+ffile = open(filename,'r')
+
+
+
+
+text_file = open(filename2, 'w')
+
+text_file.write
+
+wb = load_workbook(filename1)
+sheet = wb.get_sheet_by_name('Лист1')
+mtrx = np.zeros([100,3])
+# Retrieve cell value 
+sheet.cell(row=1, column=2).value
+for row in sheet['A1':'C35']:
+    string = ''
+    for cell in row:
+        string = string + str(cell.value) + '\n'
+    text_file.write(string)
+
+
+
+print('Слово "' + word +'" встречается' if word in ffile.read() else 'Нет такого слова')
+print(ffile)
+
+text_file.close()
+
+# Print out values in column 2 
+#for i in range(1, 4):
+   #  print(i, sheet.cell(row=i, column=2).value)
+
+#print(mtrx)
