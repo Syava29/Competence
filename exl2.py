74efe9f39399ced78413d3555069a8272b8d1cd6
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-06-02 02:54:57 +0300

    Подправил поиск

--- exl2.py
+++ exl2.py
@@ -6,6 +6,7 @@ import re
 filename1 = "Компетенции.xlsx"
 filename2 = "load.txt"
 filename3 = "key_word.txt"
+filename4 = "soderzh.txt"
 
 spisok = []
 spisok_book = []
@@ -16,7 +17,7 @@ komp = input()
 filename = "load.txt"
 ffile = open(filename,'r')
 
-
+soderzh = open(filename4, 'w')
 text_file = open(filename2, 'w')
 text_file2 = open(filename3, 'w')
 
@@ -47,7 +48,8 @@ for row in sheet['E1':'E3']:
     for cell in row:
         string = string + str(cell.value) + '\n\n'
     spisok_book.append(string)
-print(spisok_book)
+
+print('Список литературы : '+str(spisok_book))
 
 
 for row in sheet['F1':'F3']:
@@ -55,8 +57,8 @@ for row in sheet['F1':'F3']:
     for cell in row:
         string = string + str(cell.value) + '\n\n'
     spisok_description.append(string)
-    
-print(spisok_description)
+    soderzh.write(string)
+
 
 
 text_file.close()
@@ -67,23 +69,37 @@ with open('load.txt') as f:
 if komp == "УК-1":
     search_text = spisok[0]
     allres = re.findall(search_text, s)
+    k1 = 0
     if search_text == allres[0]:
         print('К данной компетенции подходит книга : ' + spisok_book[0])
+ #for value in enumerate(spisok_book, 1):
+
+  # print('К данной компетенции подходит книга : ' + spisok_book[0])           
+     
    
+   # if search_text == allres[0]:
+      #  print('К данной компетенции подходит книга : ' + spisok_book[0])
+
 
 if komp == "УК-2":
     search_text = spisok[1]
     allres = re.findall(search_text, s)
     if search_text == allres[0]:
         print('К данной компетенции подходит книга : ' + spisok_book[1])
+
+if komp == "УК-3":
+    search_text = spisok[2]
+    allres = re.findall(search_text, s)
+    if search_text == allres[0]:
+        print('К данной компетенции подходит книга : ' + spisok_book[2])
     
 
 search_text = spisok[1]
 #search_text = r"философии"
 allres = re.findall(search_text, s)
 if allres !=0: 
-    print("OKOKOK")
+    print("Литература есть")
 
-print(allres)
+
 
 
