5baade06b58c17cef3200c3bb094bf1f1c346711
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-06-08 02:37:13 +0300

    улучшил поиск

--- exl2.py
+++ exl2.py
@@ -2,91 +2,91 @@
 from openpyxl import load_workbook
 import numpy as np
 import re 
-
+#название файлов
 filename1 = "Компетенции.xlsx"
 filename2 = "load.txt"
 filename3 = "key_word.txt"
 filename4 = "soderzh.txt"
-
+#Листы 
 spisok = []
 spisok_book = []
 spisok_description = []
 
 
-komp = input()
+komp = input()    #     ввод индикатора компетенции пользователем
+
 filename = "load.txt"
 ffile = open(filename,'r')
 
 soderzh = open(filename4, 'w')
 text_file = open(filename2, 'w')
 text_file2 = open(filename3, 'w')
-
+#       считываем данные из excel
 wb = load_workbook(filename1)
 sheet = wb['Лист1']
 mtrx = np.zeros([100,3])
 
 sheet.cell(row=1, column=2).value
-for row in sheet['A1':'D35']:
+for row in sheet['A1':'C35']:
     string = ''
     for cell in row:
         string = string + str(cell.value) + '\n\n'
     text_file.write(string)
-
+    result_main = re.findall(r'\w+', string)
+   
 
 for row in sheet['D1':'D35']:
-    string = ''
+    string1 = ''
     for cell in row:
-        string = string + str(cell.value) + '\n\n'
-    spisok.append(string)
-    text_file2.write(string)
+        string1 = string1 + str(cell.value)
+    spisok.append(string1)
 
+    result_key = re.findall(r'\w+', string1)
+    
 
 #сравниваем ключивые слова и литературу
 
 for row in sheet['E1':'E3']:
-    string = ''
+    string2 = ''
     for cell in row:
-        string = string + str(cell.value) + '\n\n'
-    spisok_book.append(string)
-
-print('Список литературы : '+str(spisok_book))
+        string2 = string2 + str(cell.value) + '\n\n'
+    spisok_book.append(string2)
+    result1 = re.findall(r'\w+', string2)
+    
+print('Список литературы : ' + str(spisok_book))
 
 
 for row in sheet['F1':'F3']:
-    string = ''
+    string3 = ''
     for cell in row:
-        string = string + str(cell.value) + '\n\n'
-    spisok_description.append(string)
-    soderzh.write(string)
-
+        string3 = string3 + str(cell.value) + '\n\n'
+    spisok_description.append(string3)
+    soderzh.write(string3)
+    result1 = re.findall(r'\w+', string3)
+    
 
 
 text_file.close()
 with open('load.txt') as f:
     s = f.read()
 
+#предлагаем лиетратуру
 
 if komp == "УК-1":
-    search_text = spisok[0]
-    allres = re.findall(search_text, s)
+    fword = re.search(r'\w+', str(spisok_description))
+    search_text = spisok[00]
+    allres = re.findall(search_text, str(spisok_description))
     k1 = 0
     if search_text == allres[0]:
-        print('К данной компетенции подходит книга : ' + spisok_book[0])
- #for value in enumerate(spisok_book, 1):
+        print('К данной компетенции подходит книга : ' + str(fword.group(0)))
 
-  # print('К данной компетенции подходит книга : ' + spisok_book[0])           
-     
-   
-   # if search_text == allres[0]:
-      #  print('К данной компетенции подходит книга : ' + spisok_book[0])
-
-
+#предлагаем лиетратуру
 if komp == "УК-2":
     search_text = spisok[1]
     allres = re.findall(search_text, s)
     if search_text == allres[0]:
         print('К данной компетенции подходит книга : ' + spisok_book[1])
-
+#предлагаем лиетратуру
 if komp == "УК-3":
     search_text = spisok[2]
     allres = re.findall(search_text, s)
@@ -99,6 +99,11 @@ search_text = spisok[1]
 allres = re.findall(search_text, s)
 if allres !=0: 
     print("Литература есть")
+
+
+strstr = open(filename,'r')
+
+result1 = re.findall(r'\w+', string3)
 
 
 
