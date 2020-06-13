6a1d3e3696574a1926a3e1515ed89c854f89667c
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-06-13 17:07:52 +0300

    парсинг ЗУВов на слова для дальнейшего анализа

--- exl2.py
+++ exl2.py
@@ -31,16 +31,21 @@ wb_books = load_workbook(filename_books)
 sheet_books = wb_books['Лист1']
 sheet = wb['Лист1']
 
-
+# Парсим Содержание книг
 sheet_books.cell(row=2, column=1).value
 for row in sheet_books['A1':'CO3']:
     string = ''
     for cell in row:
         string = string + str(cell.value) + '\n\n'
-    text_file.write(string)
-    result_main = re.sub(r'\d', '', string)
+        your_string = string
+        removal_list = [' и ',' а ','None' , 'Глава', ' в ']
+        for word in removal_list:
+            your_string = your_string.replace(word, '')  
+    
+    result_main = re.sub(r'\d', '', your_string)
     result_main = re.findall(r'\w+', result_main)
-    spisok_content.append(result_main)  
+    col_count_books = Counter(result_main).most_common(1)
+    spisok_content.append(col_count_books)  # Список слов из содержаний книг
 
 for row in sheet['D1':'D35']:
     string1 = ''
@@ -56,18 +61,19 @@ for row in sheet['C1':'C35']:
     for cell in row:
         zuv = zuv + str(cell.value)
         your_string = zuv
-        removal_list = ['Знать','Уметь','Владеть']
+        removal_list = ['Знать','Уметь','Владеть', ' и ', 'для', 'None', ' в ', ' на ', 'знать']
         for word in removal_list:
             your_string = your_string.replace(word, '')   
         
         result_key = re.findall(r'\w+', your_string)
-        col_count = Counter(result_key).most_common(3)    
+        col_count = Counter(result_key).most_common(5)    
         #sorted(Counter(result_key).elements())
         #print(sorted(Counter(result_key).elements()))
-        print(col_count)
-        
-        spisok_zuv.append(result_key)
-
+        #print(col_count)
+        result_main23 = re.sub(r'\d', '', str(col_count))
+        res_res = re.findall(r'\w+', result_main23)
+        spisok_zuv.append(res_res) # Список слов из ЗУВ для каждой компетенции для сравнния с литературой
+        print(res_res)
 
 
 
@@ -115,5 +121,5 @@ strstr = open(filename,'r')
 #result1 = re.findall(r'\w+', string1)
 #result111=list(set(col_count) & set(spisok))
 
-print(result111)
+#print(result111)
 
