5eac95a7a26c3f33a5dc4234bc7b8bfa246600cb
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-07-07 20:41:55 +0300

    Minor update

--- exl2.py
+++ exl2.py
@@ -1,19 +1,16 @@
-#import openpyxl as excl
 from collections import Counter
 from openpyxl import load_workbook
-import numpy as np
 import re 
 from tkinter import * 
 from tkinter import scrolledtext  
+import numpy as np
 
-#название файлов
+
+""" Название файлов """
 filename1 = "Компетенции.xlsx"
 filename_books = "spisok_knig.xlsx"
 
-#filename2 = "load.txt"
-#filename3 = "key_word.txt"
-#filename4 = "soderzh.txt"
-#Листы 
+""" Списки """
 spisok = []
 spisok_book = []
 spisok_description = []
@@ -21,23 +18,15 @@ spisok_zuv = []
 spisok_content = []
 spisok_nazv = []
 spisok_komp = []
-#print('Введите индикатор ')
-#input_user_komp = input()    #     ввод индикатора компетенции пользователем
+description_zuv = []
 
-#filename = "load.txt"
-#ffile = open(filename,'r')
-
-#soderzh = open(filename4, 'w')
-#text_file = open(filename2, 'w')
-#text_file2 = open(filename3, 'w')
-
-#       считываем данные из excel
+""" Считываем данные из excel """
 wb = load_workbook(filename1)
 wb_books = load_workbook(filename_books)
 sheet_books = wb_books['Лист1']
 sheet = wb['Лист1']
 
-# Загружаем ЗУВы и разбиваем их на слова, а также удаляем Знать, Уметь, Владеть
+""" Загружаем ЗУВы и разбиваем их на слова, а также удаляем Знать, Уметь, Владеть """
 for row in sheet['C1':'C35']:
     zuv = ''
     for cell in row:
@@ -46,12 +35,12 @@ for row in sheet['C1':'C35']:
         removal_list = ['Знать','Уметь','Владеть', ' и ', 'для', 'None', ' в ', ' на ',' по ', 'знать', 'Задача', 'Лабораторная', 'Практикум', 'Краткое', 'cодержание', 'Задачи', 'Вариант','Основные', ' с ', ' к ', ' при ']
         for word in removal_list:
             your_string = your_string.replace(word, '')   
-        
+    description_zuv.append(your_string)    
     result_key = re.findall(r'\w+', your_string)
     col_count = Counter(result_key).most_common(5)   
     result_main23 = re.sub(r'\d', '', str(col_count))
     res_res = re.findall(r'\w+', result_main23) 
-    spisok_zuv.append(res_res) # Список слов из ЗУВ для каждой компетенции для сравнния с литературой    
+    spisok_zuv.append(res_res)  #Список слов из ЗУВ для каждой компетенции для сравнния с литературой 
 
 for row in sheet['A1':'A35']:
     string = ''
@@ -61,7 +50,7 @@ for row in sheet['A1':'A35']:
 
     spisok_komp.append(your_string)
 
-# Парсим Содержание книг
+""" Парсим содержание книг """
 sheet_books.cell(row=2, column=1).value
 for row in sheet_books['A1':'HK19']:
     string = ''
@@ -80,7 +69,7 @@ for row in sheet_books['A1':'HK19']:
     testtt = Counter(result_main) 
     spisok_content.append(res_main)  # Список слов из содержаний книг
 
-# список названий
+""" Cписок названий """
 for row in sheet_books['A1':'A19']:
     string = ''
     for cell in row:
@@ -89,9 +78,10 @@ for row in sheet_books['A1':'A19']:
 
     spisok_nazv.append(your_string)
 
-# проверка совпадений самых популярных слов из ЗУВа с каждым содержанием книги
+""" проверка совпадений самых популярных слов из ЗУВа с каждым содержанием книги """
 def clicked():
     txtscrol.delete(1.0, END)
+    txt_description.delete(1.0, END)
     i = 0
     k = 0
     #res = "Привет {}".format(txt.get())  
@@ -106,6 +96,7 @@ def clicked():
                 #print(spisok_nazv[k])
                 if txt.get() == spisok_komp[i]:
                     txtscrol.insert(INSERT, 'Для данной компетенции подходит кинга: "' + str(spisok_nazv[k]) + "\n\n")
+                    txt_description.insert(INSERT, str(description_zuv[i]) + "\n\n")
                     #lbl.configure(text='Для данной компетенции подходит кинга: "' + str(spisok_nazv[k]) + '"')
                     #print('Для данной компетенции подходит кинга: "' + str(spisok_nazv[k]) + '"')    
             k = k + 1
@@ -113,23 +104,25 @@ def clicked():
         k = 0
         i = i + 1
 
-# визуализация   
+""" Визуализация """  
 window = Tk()  
 window.title("Подбор литературы по компетенциям")  
-window.geometry('1500x800')  
+window.geometry('1500x500')  
 lbl_main = Label(window, text = 'Шаг 1')
 lbl_main.grid(column=0, row=0)
 lbl_main2 = Label(window, text = 'Шаг 2 - нажмите на кнопку')
 lbl_main2.grid(column=3, row=0) 
-lbl = Label(window, text="Введите код компетенции в текстовое поле (УК-1 ... ПК-23)", font=("Arial Bold", 10))  
+lbl = Label(window, text="Выберите компетенцию для которой необходимо подобрать литературу", font=("Arial Bold", 10))  
 lbl.grid(column=0, row=1)  
 txt = Entry(window,width=20)  
-txt.grid(column=2, row=1)   
+txt.grid(column=2, row=1)
+lbl_description = Label(window, text = 'Знать, уметь, владеть')
+lbl_description.grid(column=3, row=3)
+txt_description = scrolledtext.ScrolledText(window, width=100, height=10)
+txt_description.grid(column=3, row=4) 
 txtscrol = scrolledtext.ScrolledText(window, width=100, height=10)  
 txtscrol.grid(column=3, row=2)
 btn = Button(window, text="Подобрать литературу", command=clicked) 
 btn.grid(column=3, row=1)  
  
-
 window.mainloop()
-
